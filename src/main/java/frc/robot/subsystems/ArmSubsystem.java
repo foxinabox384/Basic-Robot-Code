74eb00d0b704baffeff2996c39a214ae0ed773fd
foxinabox384 <115041785+foxinabox384@users.noreply.github.com>
2023-12-03 22:09:17 -0800

    Add files via upload

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -12,14 +12,21 @@ public class ArmSubsystem {
         this.Motor1 = new CANSparkMax(PortConstants.PORT_1, MotorType.kBrushless);
         this.Motor2 = new CANSparkMax(PortConstants.PORT_2, MotorType.kBrushless);
     }
-    public void Motor1_Speed() {
-        this.Motor1.set(0);
+    public void Motor1_Speed(int speed) {
+        this.Motor1.set(speed);
     }
-    public void Motor2_Speed() {
-        this.Motor2.set(2);
+    public void Motor2_Speed(int speed) {
+        this.Motor2.set(speed);
     }
-    public void Set_Both() {
-        this.Motor1_Speed();
-        this.Motor2_Speed();
+    public void Set_Both(int speed1, int speed2) {
+        this.Motor1_Speed(speed1);
+        this.Motor2_Speed(speed2);
     }
+
+    //HW 2 below 
+
+    public void stopAllMotors(){
+        this.Set_Both(0, 0);
+    }
+
 }
